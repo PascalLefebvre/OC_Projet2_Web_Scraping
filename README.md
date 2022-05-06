3594fab29cdaf085c22b67ff40a4bed22f9f24a9
Pascal Lefebvre <skrova2017@gmail.com>
2022-05-06 12:08:43 +0200

    Extraction dans un fichier CSV des informations de tous les livres d'une catégorie

--- README.md
+++ README.md
@@ -3,11 +3,16 @@ Cursus Openclassrooms "Développeur d'applications - Python" (12 mois)
 
 Projet 2 : Utilisez les bases de Python pour l'analyse de marché (en utilisant le "web scraping")
 
-	Pour une importante librairie en ligne spécialisée dans les livres d'occasion, développement d'une version bêta d'une application de suivi des prix des livres d'occasion sur des sites web concurrents. Dans ce projet, suivi des prix des livres chez Books to Scrape, un revendeur de livres en ligne. En pratique, dans cette version bêta, le programme n'effectue pas une véritable surveillance en temps réel des prix sur la durée. Il s'agit simplement d'une application exécutable à la demande visant à récupérer les prix au moment de son exécution.
+	Pour une importante librairie en ligne spécialisée dans les livres d'occasion, développement d'une version bêta d'une
+	application de suivi des prix des livres d'occasion sur des sites web concurrents. Dans ce projet, suivi des prix des
+	livres chez Books to Scrape, un revendeur de livres en ligne. En pratique, dans cette version bêta, le programme
+	n'effectue pas une véritable surveillance en temps réel des prix sur la durée. Il s'agit simplement d'une application
+	exécutable à la demande visant à récupérer les prix au moment de son exécution.
 
 2ème étape réalisée :
 
-	Visite de la première page de la catégorie de livres "Sequential Art" sur le site de Books to Scrape, extraction et stockage des informations suivantes ...
+	Visite toutes les pages de la catégorie de livres "Sequential Art" sur le site de Books to Scrape,
+	extrait et stocke, pour chaque livre, les informations suivantes ...
 
 	    product_page_url
 	    universal_ product_code (upc)
--- scriptBooksToScrape.py
+++ scriptBooksToScrape.py
@@ -1,3 +1,4 @@
+from nis import cat
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -5,16 +6,16 @@ import re
 
 urlScrape = "http://books.toscrape.com"
 # bookUrl = "http://books.toscrape.com/catalogue/vampire-knight-vol-1-vampire-knight-1_93/index.html"
-categoryUrl = "http://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html"
+categoryUrlPrefix = "http://books.toscrape.com/catalogue/category/books/sequential-art_5/"
+categoryUrlSuffix = "index.html"
 csvHead = ['product_pageBook_url', 'universal_product_code', 'title', 'price_including_tax',
     'price_excluding_tax', 'number_available', 'product_description', 'category',
     'review_rating', 'image_url']
 
+# Extract the required information from the book web page
 def extractBookInformation(url):
     book = [url]
-
     pageBook = requests.get(url)
-
     if pageBook.status_code == requests.codes.ok:
         soupBook = BeautifulSoup(pageBook.content, 'html.parser')
     else:
@@ -28,8 +29,12 @@ def extractBookInformation(url):
     tableLines = soupBook.find('table', class_ = "table table-striped")
     for line in tableLines.find_all('td'):
         productInformation.append(line.string)
+    
+    # Delete useless information
     productInformation.pop(1)
     productInformation.pop(3)
+    
+    # Extract the quantity of books available
     if productInformation[3].find('available') != -1:
         match = re.search(r'\d+', productInformation[3])
         if match:
@@ -44,7 +49,7 @@ def extractBookInformation(url):
     productPage = soupBook.find('article', class_ = 'product_page').find('p', class_ = '')
     productDescription = productPage.string.replace(',', '')
 
-    # Extract category (last link in the list)
+    # Extract category (last link in the concerned list)
     links = soupBook.find('ul', class_ = 'breadcrumb').find_all('a')
     for link in links:
         category = link.string
@@ -53,7 +58,7 @@ def extractBookInformation(url):
     imageUrl = soupBook.find('div', class_ = "item active").find('img')['src'].replace('../..', '')
     imageUrl = urlScrape + imageUrl
 
-    # book filled with extracted data book
+    # "book" filled with extracted data book
     book.append(productInformation[0])
     book.append(title)
     book.append(productInformation[2])
@@ -66,25 +71,32 @@ def extractBookInformation(url):
 
     return book
 
-# Extract books informations from a specific category
-
-pageCategory = requests.get(categoryUrl)
-
-if pageCategory.status_code == requests.codes.ok:
-    soupCategory = BeautifulSoup(pageCategory.content, 'html.parser')
-else:
-    print("Error : ", pageCategory.status_code)
-    exit()
-
-# Create CSV file and save book information inside
-
+# Create CSV file and write header line inside
 with open("data/categories/Sequential_Art.csv", 'w') as file_csv:
     writer = csv.writer(file_csv, delimiter=',')
     writer.writerow(csvHead)
     
-    booksUrl = soupCategory.find('ol', class_ = 'row').find_all('h3')
-    for bk in booksUrl:
-        bookUrl = urlScrape + bk.find('a')['href'].replace('../../..', '/catalogue')
-        bookInformation = extractBookInformation(bookUrl)
-        writer.writerow(bookInformation)
-    
+    # Extract books information from all the web pages of a specific category
+    categoryUrl = categoryUrlPrefix + categoryUrlSuffix
+    extractPage = True
+    while extractPage:
+        pageCategory = requests.get(categoryUrl)
+        if pageCategory.status_code == requests.codes.ok:
+            soupCategory = BeautifulSoup(pageCategory.content, 'html.parser')
+        else:
+            print("Error : ", pageCategory.status_code)
+            exit()
+
+        # Write books information from one web page in the CSV file
+        booksUrl = soupCategory.find('ol', class_ = 'row').find_all('h3')
+        for bk in booksUrl:
+            bookUrl = urlScrape + bk.find('a')['href'].replace('../../..', '/catalogue')
+            bookInformation = extractBookInformation(bookUrl)
+            writer.writerow(bookInformation)
+        
+        # For the category, create the URL of the next web page if exists
+        if soupCategory.find('li', class_ = 'next') != None:
+            linkNext = soupCategory.find('li', class_ = "next").find('a')['href']
+            categoryUrl = categoryUrlPrefix + linkNext
+        else:
+            extractPage = False
